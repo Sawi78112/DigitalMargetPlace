452981dfb7b4c362306a2c3f52043ef3a31cbac3
John Carlo Asilo <jcarloasilo@gmail.com>
2025-05-26 19:12:27 +0800

    feat(ui): add reusable Page Header component for consistent page titles

---
+++ src/lib/components/header/header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { ArrowLeftIcon } from 'lucide-svelte';
+
+	export let title: string;
+</script>
+
+<header class="flex items-center gap-3 py-5">
+	<Button
+		class="flex h-7 w-7 items-center justify-center rounded-full bg-gray-100 p-0 text-gray-700 hover:bg-gray-300"
+		onclick={() => history.back()}
+	>
+		<ArrowLeftIcon class="h-4 w-4" />
+	</Button>
+
+	<h1 class="text-xl">{title}</h1>
+</header>
