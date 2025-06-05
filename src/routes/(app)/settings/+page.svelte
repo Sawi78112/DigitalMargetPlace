282d4579285b58e36a69b4fbd2fa99ef3648f64d
John Carlo Asilo <jcarloasilo@gmail.com>
2025-06-05 21:21:02 +0800

    feat: add profile password form to settings page

--- src/routes/(app)/settings/+page.svelte
+++ src/routes/(app)/settings/+page.svelte
@@ -1,1 +1,13 @@
-<h1>Settings</h1>
+<script lang="ts">
+	import { ProfilePasswordForm } from '$modules/profile/profile-form';
+	import { Separator } from '$lib/components/ui/separator';
+</script>
+
+<div class="space-y-4">
+	<div>
+		<h3 class="text-lg font-medium">Profile</h3>
+		<p class="text-sm text-muted-foreground">Update your details.</p>
+	</div>
+	<Separator />
+	<ProfilePasswordForm />
+</div>
