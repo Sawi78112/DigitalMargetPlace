394736370d12da3130ec2049a858525dd4d8ee85
John Carlo Asilo <jcarloasilo@gmail.com>
2025-06-10 22:03:44 +0800

    feat: add new/update profile schema

--- src/modules/profile/schemas.ts
+++ src/modules/profile/schemas.ts
@@ -13,3 +13,23 @@ export const updatePasswordSchema = z.object({
 });
 
 export type UpdatePasswordSchema = Infer<typeof updatePasswordSchema>;
+
+export const newProfileSchema = z.object({
+	first_name: z.string().min(1, 'First name is required'),
+	middle_name: z.string().min(1, 'Middle name is required'),
+	last_name: z.string().min(1, 'Last name is required'),
+	birthdate: z
+		.string()
+		.min(1, 'Birthdate is required')
+		.regex(/^\d{4}-\d{2}-\d{2}$/, 'Invalid date format (YYYY-MM-DD)'),
+	gender: z.string().min(1, 'Gender is required'),
+	user_id: z.string().uuid()
+});
+
+export type NewProfileSchema = Infer<typeof newProfileSchema>;
+
+export const updateProfileSchema = newProfileSchema.extend({
+	id: z.string().uuid({ message: 'Invalid profile ID' })
+});
+
+export type UpdateProfileSchema = Infer<typeof updateProfileSchema>;
