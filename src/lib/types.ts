01559adf7342bb8788bfdd8717b2f822e2603a8e
John Carlo Asilo <jcarloasilo@gmail.com>
2025-06-10 22:00:31 +0800

    fear: add profile type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -29,6 +29,18 @@ export type User = {
 	updated_at: string;
 };
 
+export type Profile = {
+	id: string;
+	first_name: string;
+	middle_name: string;
+	last_name: string;
+	birthdate: string;
+	gender: string;
+	created_at: string;
+	updated_at: string;
+	user_id: string;
+};
+
 export type ForgotPasswordRequestResult = {
 	id: string;
 };
