acba94469c66419064ee5ee66b0a0fd16aefe25e
John Carlo Asilo <jcarloasilo@gmail.com>
2025-06-27 19:55:05 +0800

    refactor(products): rename products/create to add-new-product and add the refactored product form

---
+++ src/routes/(app)/products/add-new-product/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { NewProductForm } from '$modules/products/product-form';
+</script>
+
+<div class="space-y-5">
+	<div class="flex items-center justify-between">
+		<NewProductForm />
+	</div>
+</div>
