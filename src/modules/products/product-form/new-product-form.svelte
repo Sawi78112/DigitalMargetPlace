b9307a126cc36633e653bf6c7dafd59313be6156
John Carlo Asilo <jcarloasilo@gmail.com>
2025-06-27 19:47:11 +0800

    refactor(form):  add photos, title, price, cover and visibility for product form

--- src/modules/products/product-form/new-product-form.svelte
+++ src/modules/products/product-form/new-product-form.svelte
@@ -13,6 +13,9 @@
 	import Textarea from '$lib/components/ui/textarea/textarea.svelte';
 	import { MultipleDropdown } from '$lib/components/dropdown';
 	import { reactiveQueryArgs } from '$lib/components/svelte-query';
+	import { Label } from '$lib/components/ui/label';
+	import { RadioGroup, RadioGroupItem } from '$lib/components/ui/radio-group';
+	import { Button } from '$lib/components/ui/button';
 
 	type Props = {
 		product?: Product | null;
@@ -58,9 +61,11 @@
 	$effect(() => {
 		if (product) {
 			$formData = {
-				name: product.name,
+				title: product.title,
 				description: product.description,
-				categories: product.categories ?? []
+				price: product.price,
+				categories: product.categories ?? [],
+				visibility: product.visibility
 			};
 		}
 	});
@@ -70,32 +75,137 @@
 	method="POST"
 	use:enhance
 	id="product-record-form"
-	class="grid w-full grid-cols-4 gap-x-4 gap-y-4"
+	class="grid w-full grid-cols-6 gap-x-6 gap-y-6"
 >
-	<Form.Field {form} name="name" class="col-span-4 sm:col-span-2">
+	<Label>Product Photos</Label>
+	<Form.Field {form} name="photos" class="col-span-6">
 		<Form.Control>
-			<Form.Label>Name</Form.Label>
-			<Input type="text" bind:value={$formData.name} />
+			<Label
+				class="hover:bg-muted relative flex w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed px-4 py-12 text-center transition"
+			>
+				<img
+					src="https://placehold.co/400"
+					alt="Upload Icon"
+					class="mx-auto h-20 w-20 text-blue-400"
+				/>
+				<p class="text-sm font-medium">
+					Upload Image <span class="text-gray-500">(0/10)</span>
+				</p>
+				<p class="mt-1 text-xs text-gray-500">Drag and drop your file here, or click to browse.</p>
+				<Input
+					id="productPhotos"
+					type="file"
+					name="productPhotos"
+					multiple
+					accept="image/*"
+					class="absolute inset-0 cursor-pointer opacity-0"
+				/>
+			</Label>
 		</Form.Control>
 		<Form.FieldErrors />
 	</Form.Field>
 
-	<Form.Field {form} name="categories" class="col-span-4 sm:col-span-2">
+	<Form.Field {form} name="title" class="col-span-4 sm:col-span-2">
+		<Form.Control>
+			<Form.Label>Title</Form.Label>
+			<Input type="text" bind:value={$formData.title} />
+		</Form.Control>
+		<Form.FieldErrors />
+	</Form.Field>
+
+	<Form.Field {form} name="price" class="col-span-4 sm:col-span-2">
+		<Form.Control>
+			<Form.Label>Price</Form.Label>
+			<Input type="number" bind:value={$formData.price} />
+		</Form.Control>
+		<Form.FieldErrors />
+	</Form.Field>
+
+	<Form.Field {form} class="col-span-4 sm:col-span-2" name="categories">
 		<Form.Control>
 			<Form.Label>Category</Form.Label>
 			<MultipleDropdown
-				placeholder="Select Categories"
-				bind:selectedValue={$formData.categories}
+				placeholder="categories"
+				bind:selectedValue={$formData.gender}
 				options={categoryNames}
 			/>
 		</Form.Control>
 		<Form.FieldErrors />
 	</Form.Field>
 
-	<Form.Field {form} name="description" class="col-span-4 sm:col-span-2">
+	<Label>Cover</Label>
+	<Form.Field {form} name="cover" class="col-span-6">
+		<Form.Control>
+			<Label
+				class="hover:bg-muted relative flex w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed px-4 py-12 text-center transition"
+			>
+				<img
+					src="https://placehold.co/400"
+					alt="Upload Icon"
+					class="mx-auto h-20 w-20 text-blue-400"
+				/>
+				<p class="text-sm font-medium">
+					Upload Image <span class="text-gray-500">(0/10)</span>
+				</p>
+				<p class="mt-1 text-xs text-gray-500">Drag and drop your file here, or click to browse.</p>
+				<Input
+					id="productPhotos"
+					type="file"
+					name="productPhotos"
+					multiple
+					accept="image/*"
+					class="absolute inset-0 cursor-pointer opacity-0"
+				/>
+			</Label>
+		</Form.Control>
+		<Form.FieldErrors />
+	</Form.Field>
+
+	<Form.Field {form} name="description" class="col-span-6">
 		<Form.Control>
 			<Form.Label>Description</Form.Label>
 			<Textarea bind:value={$formData.description} />
+		</Form.Control>
+		<Form.FieldErrors />
+	</Form.Field>
+
+	<Form.Field {form} name="visibility" class="col-span-6">
+		<Form.Control>
+			<Form.Label>Visibility</Form.Label>
+			<RadioGroup bind:value={$formData.visibility} class="mt-2 flex flex-col gap-3">
+				<div class="flex items-start space-x-3">
+					<RadioGroupItem id="visible" value="visible" />
+					<Label for="visible" class="flex flex-col">
+						<p>
+							<span class="font-medium">Visible</span> -
+							<span class="text-muted-foreground text-sm">Everyone can see this content.</span>
+						</p>
+					</Label>
+				</div>
+				<div class="flex items-start space-x-3">
+					<RadioGroupItem id="invisible" value="invisible" />
+					<Label for="invisible" class="flex flex-col">
+						<p>
+							<span class="font-medium">Invisible</span> -
+							<span class="text-muted-foreground text-sm"
+								>Nobody except you can see this content.</span
+							>
+						</p>
+					</Label>
+				</div>
+				<div class="flex items-start space-x-3">
+					<RadioGroupItem id="unlisted" value="unlisted" />
+					<Label for="unlisted" class="flex flex-col">
+						<p>
+							<span class="font-medium">Unlisted</span> -
+							<span class="text-muted-foreground text-sm"
+								>Only people with the direct link can see this. It won't be listed alongside other
+								content on your store.</span
+							>
+						</p>
+					</Label>
+				</div>
+			</RadioGroup>
 		</Form.Control>
 		<Form.FieldErrors />
 	</Form.Field>
@@ -119,15 +229,22 @@
 		{/if}
 	</div>
 
-	<div class="col-span-4 mt-4 flex justify-end">
+	<div class="col-span-6 mt-2 mb-6 flex justify-end gap-3">
+		<Button
+			href="/products"
+			class="inline-flex items-center rounded-md border border-gray-300 bg-white px-6 py-2 text-sm font-medium text-gray-700 shadow-sm transition hover:bg-gray-50"
+		>
+			Cancel
+		</Button>
+
 		<Form.Button
 			disabled={$createProductMutation.isPending || $createProductMutation.isPaused}
-			class="px-6 py-2"
+			class="inline-flex items-center rounded-md  px-6 py-2 text-sm font-medium text-white transition disabled:opacity-50"
 		>
 			{#if $createProductMutation.isPending || $createProductMutation.isPaused}
 				<LoaderIcon class="mr-2 h-5 w-5 animate-spin" />
 			{/if}
-			Add
+			Add Product
 		</Form.Button>
 	</div>
 </form>
