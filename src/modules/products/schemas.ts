d90b5491e35d9a85db6973307a3576e18119e910
John Carlo Asilo <jcarloasilo@gmail.com>
2025-07-01 21:53:31 +0800

    feat(schema):  extend newProductSchema to enable photo fields

--- src/modules/products/schemas.ts
+++ src/modules/products/schemas.ts
@@ -2,11 +2,25 @@ import { z } from 'zod';
 import type { Infer } from 'sveltekit-superforms';
 
 export const newProductSchema = z.object({
-	title: z.string().min(1, 'required'),
-	description: z.string().min(1, 'required'),
-	price: z.number().min(0, 'price must be non-negative'),
-	categories: z.array(z.string()).min(1, 'at least one category is required'),
-	visibility: z.string().min(1, 'required')
+	title: z.string().min(1, { message: 'Title is required' }),
+	description: z.string().min(1, { message: 'Description is required' }),
+	price: z.number().min(0, { message: 'Price must be non-negative' }),
+	categories: z.array(z.string()).min(1, { message: 'At least one category is required' }),
+	visibility: z.string().min(1, { message: 'Visibility is required' }),
+	cover_photo: z
+		.object({
+			image_url: z.string().min(1, { message: 'Image URL is required' })
+		})
+		.optional(),
+	product_photos: z
+		.array(
+			z.object({
+				image_url: z.string().min(1, { message: 'Image URL is required' })
+			})
+		)
+		.max(10, { message: 'You can add up to 10 product photos' })
+		.optional()
+		.default([])
 });
 
 export const updateProductSchema = newProductSchema.extend({
