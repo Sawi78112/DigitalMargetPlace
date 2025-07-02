5a8a28d4f03395af8be079ba4ad316dc4e1caa40
John Carlo Asilo <jcarloasilo@gmail.com>
2025-07-02 23:33:54 +0800

    refactor: restyle /login page

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,35 +1,17 @@
 <script lang="ts">
-	import { UserRoundIcon } from 'lucide-svelte';
-
 	import { LoginForm } from '$modules/auth/login-form';
 </script>
 
-<div class="relative container mx-auto flex h-screen items-center justify-center">
-	<div class="lg:p-8">
-		<div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-			<div class="flex flex-col items-center gap-y-4 pt-10">
-				<div class="rounded-full border bg-gray-50 p-4">
-					<div class="rounded-full border bg-white p-3.5">
-						<UserRoundIcon class="size-7 text-gray-500" />
-					</div>
-				</div>
-				<div class="text-center">
-					<div class="text-2xl font-semibold">Login to your account</div>
-					<div class="text-gray-600">Enter your details to login</div>
-				</div>
-			</div>
+<div
+	class="relative container h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0"
+>
+	<div class="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
+		<div class="absolute inset-0 bg-[url('/img/dmp-bg.jpg')] bg-cover bg-right bg-no-repeat"></div>
+	</div>
+
+	<div class="flex min-h-screen items-center justify-center">
+		<div class="w-full max-w-md">
 			<LoginForm />
-			<p class="text-muted-foreground px-8 text-center text-sm">
-				By clicking continue, you agree to our
-				<a href="/terms" class="hover:text-primary underline underline-offset-4">
-					Terms of Service
-				</a>
-				and
-				<a href="/privacy" class="hover:text-primary underline underline-offset-4">
-					Privacy Policy
-				</a>
-				.
-			</p>
 		</div>
 	</div>
 </div>
