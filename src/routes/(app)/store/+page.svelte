0b0e95e3625981b8350faf2af9461704cd990a94
John Carlo Asilo <jcarloasilo@gmail.com>
2025-07-18 18:18:01 +0800

    refactor(store): add blog tab on store page

--- src/routes/(app)/store/+page.svelte
+++ src/routes/(app)/store/+page.svelte
@@ -17,6 +17,7 @@
 	<div class="w-full">
 		<Tabs.List>
 			<Tabs.Trigger value="store">Store</Tabs.Trigger>
+			<Tabs.Trigger value="blog-posts">Blog posts</Tabs.Trigger>
 		</Tabs.List>
 	</div>
 
@@ -37,6 +38,28 @@
 					</p>
 
 					<Button href="/store/create-store">Create</Button>
+				</div>
+			</Card.Content>
+		</Card.Root>
+	</Tabs.Content>
+
+	<Tabs.Content value="blog-posts">
+		<Card.Root>
+			<Card.Content class="space-y-2">
+				<div class="flex flex-col items-center space-y-2 text-center">
+					<Button
+						class="flex h-10 w-10 items-center justify-center rounded-full bg-gray-100 p-0 text-gray-700 hover:bg-gray-300"
+						href="/store/add-new-blog-post"
+					>
+						<Plus class="h-4 w-4" />
+					</Button>
+
+					<h1 class="text-xl">Add Blog Post</h1>
+					<p class=" pb-4 text-sm text-gray-700">
+						Start Creating a New Blog Post to Share Your Insights and Expertise
+					</p>
+
+					<Button href="/store/add-new-blog-post">Add Blog Post</Button>
 				</div>
 			</Card.Content>
 		</Card.Root>
