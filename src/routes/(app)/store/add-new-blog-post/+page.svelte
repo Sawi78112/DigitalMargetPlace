6c55c4b73e2ae72d7b617d64e7b3f40c215d0a3a
John Carlo Asilo <jcarloasilo@gmail.com>
2025-07-18 18:23:53 +0800

    feat: add new blog post page

---
+++ src/routes/(app)/store/add-new-blog-post/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { DashboardHeader } from '$lib/components/header';
+	import { NewBlogForm } from '$modules/blog/blog-form';
+</script>
+
+<DashboardHeader
+	heading="Add new blog post"
+	description="To get choose your blog post title then add the content"
+	backHref="/store"
+/>
+
+<div class="space-y-5">
+	<div class="flex items-center justify-between">
+		<NewBlogForm />
+	</div>
+</div>
